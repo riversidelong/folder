9996d29a933abec59f2e04125fbb03038007cf5d
川端伸彦 <kawabatanobuhiko@kawabatanobuhiko-no-MacBook-Pro.local>
2014-05-11 09:17:32 +0900

    Initial Commit

---
+++ app/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  app
+//
+//  Created by 川端伸彦 on 2014/05/11.
+//  Copyright (c) 2014年 mikke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
